2ad0413a32a2e95e9dd0e5de813fe4d29d596f44
Krogan1987 <44030461+Krogan1987@users.noreply.github.com>
2018-12-01 12:41:20 +0700

    апдейт
    апдейт

--- lab3-0/src/main.c
+++ lab3-0/src/main.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
+
 typedef int key;
-void Sift (key a[], int i, int n) {
-	int l, r, k;
-	key t;
+
+static void sift (key a[], int i, int n) {
 	i++;
+	int l=0, r=0, k=0;
+	key t=0;
 	while ((l=2*i)<=n) {
 		r=(l+1<=n)? l+1 : i;
 		if ((a[i-1]>=a[l-1]) && (a[i-1]>=a[r-1]))
@@ -16,28 +18,32 @@ void Sift (key a[], int i, int n) {
 		i=k;
 	}
 }
-void heap_sort (key a[], int N) {
-	int i;
-	key t;
+
+static void heap_sort (key a[], int N) {
+	int i=0;
 	for (i=N/2; i>=0; i--)
-		Sift (a, i, N);
+		sift (a, i, N);
+	key t=0;
 	for (i=N-1; i>0; i--) {
 		t=a[0];
 		a[0]=a[i];
 		a[i]=t;
-		Sift (a, 0, i);
+		sift (a, 0, i);
 	}
 }
+
 int main(void) {
-	int i, N;
+	int N=0;
 	scanf("%d", &N);
 	int *arr=NULL;
-	arr=(int*) malloc (N*sizeof(int));
+	arr=malloc (N*sizeof *arr);
+	if (arr==NULL) exit (1); 
+	int i=0; 
 	for(i=0; i<N; ++i) 
 		scanf("%d", &arr[i]);
 	heap_sort (arr, N);
 	for(i=0; i<N; ++i) 
 		printf("%d ", arr[i]);
 	free(arr);
-	return EXIT_SUCCESS;
+	return 0;
 }
