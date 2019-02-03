2d4f637507377f73645069cfcab7277bdd24c74e
Krogan1987 <44030461+Krogan1987@users.noreply.github.com>
2019-02-04 01:31:06 +0700

    Бойер-Мур. Версия 2019 года.
    Проходит все тесты

--- lab1-0/src/main.c
+++ lab1-0/src/main.c
@@ -1,39 +1,47 @@
 #include <stdio.h>
 #include <string.h>					
 									
-int main (void) {	
-	char a[18], buf[256];			
+int main (void) {
+	char a[18], buf[260];		
 	fgets(a, 18, stdin);
 	size_t nread;
-	nread=fread(buf, 1, 255, stdin);
+	nread=fread(buf, 1, 256, stdin);
 	buf[nread]=0;
 	int N=0, M=0;										
 	M=(int)strlen(a);
-	N=(int)strlen(buf);
+	N=(int)strlen(buf);	
 	if (N<2) return 0;
-	int i=0;  			
-	i=M-2;
-	int j=0, k=0;
+	int count=0;
 	do {
-		j=M-2;
-		k=i;
-		printf("%d ", k+1);
-		int sum=0;
-		while ((j>0) && (a[j]==buf[k])) {
-			k--;
-			j--;
-			printf("%d ", k+1);
-			sum++;
-		}
-		if (j==0) i=i+M-1;	
-		else {
-			int x=3;
-			while((x<=M) && (buf[k+sum]!=a[M-x])) {
-				x++;
-				i++;
+		int i=0;  			
+		i=M-2;
+		int j=0, k=0;
+		do {
+			j=M-2;
+			k=i;
+			printf("%d ", k+1+count);
+			int sum=0;
+			while ((j>0) && (a[j]==buf[k])) {
+				k--;
+				j--;
+				printf("%d ", k+1+count);
+				sum++;
 			}
-			i=i+1;
-		}
-	} while (i<N);
+			if (j==0) i=i+M-1;	
+			else {
+				int x=3;
+				while((x<=M) && (buf[k+sum]!=a[M-x])) {
+					x++;
+					i++;
+				}
+				i=i+1;
+			}
+		} while (i<N);
+		size_t nread;
+		nread=fread(buf, 1, 256, stdin);
+		buf[nread]=0;
+		N=(int)strlen(buf);
+		count+=256;
+	} while (N>0);
 	return 0;
 }
